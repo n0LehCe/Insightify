001092eaff8f497a6f189fc21b902a625f1b0d7e
Bingchen Yang <bingchen.yang@mail.utoronto.ca>
2025-02-10 15:19:25 -0500

    minor update

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ test_case = int(input("Enter 1 for pure text test, 2 for text/table/image test")
 input_dir = ""
 output_dir = "./test_output_md/"
 if test_case == 1:
-    extractor = InsightifyExtractor(input_dir="./test_PDF/PDF_pure_text", output_dir=output_dir, result_type="markdown",
+    extractor = InsightifyExtractor(input_dir="./test_pdf/pdf_pure_text", output_dir=output_dir, result_type="markdown",
                                     api_key=os.getenv("LLAMA_API_KEY"))
 elif test_case == 2:
-    extractor = InsightifyExtractor(input_dir="./test_PDF/PDF_text_table_image", output_dir=output_dir,
+    extractor = InsightifyExtractor(input_dir="./test_pdf/pdf_text_table_image", output_dir=output_dir,
                                     result_type="markdown", api_key=os.getenv("LLAMA_API_KEY"))
 extractor.dump_to_markdown_helper()
