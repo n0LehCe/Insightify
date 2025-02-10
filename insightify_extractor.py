f320b7f0aea3880687019d47073eb1c466619374
Bingchen Yang <bingchen.yang@mail.utoronto.ca>
2025-02-10 12:38:03 -0500

    refactor parse.py's name to insightify_extractor.py. integrate it into a Class

---
+++ insightify_extractor.py
@@ -0,0 +1,32 @@
+import os
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+from dotenv import load_dotenv
+
+
+class InsightifyExtractor:
+    def __init__(self, input_dir, result_type="text", api_key=None):
+        self.input_dir = input_dir
+        self.result_type = result_type
+        self.api_key = api_key or os.getenv("LLAMA_API_KEY")
+        self.parser = LlamaParse(result_type=self.result_type, api_key=self.api_key)
+        self.file_extractor = {".pdf": self.parser}
+        self.reader = SimpleDirectoryReader(input_dir=self.input_dir, file_extractor=self.file_extractor)
+
+    def load_and_extract_content(self):
+        documents = self.reader.load_data()
+        extracted_contents = []
+        for document in documents:
+            extracted_contents.append(document.text)
+        return extracted_contents
+
+    def print_extracted_content(self):
+        contents = self.load_and_extract_content()
+        for content in contents:
+            print(content)
+
+
+# usage:
+# load_dotenv()
+# pdf_extractor = InsightifyExtractor(input_dir="./test_PDF", result_type="markdown")
+# pdf_extractor.print_extracted_content()
